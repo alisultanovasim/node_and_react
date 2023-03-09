f2edea8952f0f06aa583c1edcdb4307efae92746
alisultanovasim <asim.alisultanov@timesoft.az>
2023-03-09 10:32:53 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p style={{color : 'red',fontSize : 50}}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Form/>
         </a>
       </header>
     </div>
